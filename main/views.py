8c68964bf70ee9e99b8dea7af4dd9be7cee81f97
Antttooon <anttoon@bk.ru>
2018-05-24 15:48:41 +0300

    front-end

--- main/views.py
+++ main/views.py
@@ -2,37 +2,27 @@ from django.shortcuts import render, HttpResponse
 from mimetypes import MimeTypes
 import os
 import json
-
 import requests
 
 
-
-
-
-
 def index(request):
-
-    cl_platform =  str(request.user_agent.os.family)
+    cl_platform = str(request.user_agent.os.family)
     cl_ver = str(request.user_agent.os.version_string)
-
-    query_s = cl_platform + ' '+ cl_ver
-
-    print(query_s)
+    query_s = cl_platform + ' ' + cl_ver
 
     query = {'query': query_s,
-             'references':True,
-             'sort':'published',
-             'fields':['id']
+             'references': True,
+             'sort': 'published',
+             'fields': ['id', 'title', 'type', 'bulletinFamily', 'published', 'modified', 'href'],
              }
-    headers = {'accept':'application/json','Content-Type':'application/x-www-form-urlencoded'}
-    res = requests.post('https://vulners.com/api/v3/search/lucene',  data=json.dumps(query), headers=headers)
-
+    headers = {'accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}
+    res = requests.post('https://vulners.com/api/v3/search/lucene', data=json.dumps(query), headers=headers)
+    print(type(res.text))
     file_name = 'file.txt'
-    with open(file_name,'w') as f:
-        json.dump(res.json(),f, indent=8)
-    return render(request, 'index.html',{'query':query_s,'output':res.text})
+    with open(file_name, 'w') as f:
+        json.dump(res.json(), f, indent=3)
 
-
+    return render(request, 'index.html', {'query': query_s, 'output': res.text})
 
 
 def save_file(request):
@@ -47,6 +37,6 @@ def save_file(request):
 
         response['Content-Type'] = file_type
         response['Content-Length'] = str(os.stat(file_name).st_size)
-        response['Content-Disposition'] = "attachment; filename="+file_name
+        response['Content-Disposition'] = "attachment; filename=" + file_name
 
         return response
